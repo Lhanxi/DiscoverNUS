0f62228e884cdeb43cba3f11266bf8d7c808669d
iceflakes12 <106156502+iceflakes12@users.noreply.github.com>
2024-07-12 13:24:53 +0800

    Add files via upload

---
+++ DiscoverNUS/DiscoverNUS/MultiPlayer/JoinPartyView.swift
@@ -0,0 +1,118 @@
+//
+//  JoinPartyView.swift
+//  DiscoverNUS
+//
+//  Created by Leung Han Xi on 20/6/24.
+//
+
+import SwiftUI
+import Firebase
+import FirebaseFirestore
+
+@MainActor
+final class JoinPartyViewModel : ObservableObject {
+    @Published var partyCode = ""
+    @Published var errorMessage: String?
+    
+    func joinParty() async {
+        do {
+            let user = try AuthenticationManager.shared.getAuthenticatedUser()
+            let userID = user.uid
+            
+            let db = Firestore.firestore()
+            
+            // Check if the party exists
+            let partyRef = db.collection("Teams").document("DefaultTeam").collection("Parties").document(partyCode)
+            let partySnapshot = try await partyRef.getDocument()
+            
+            guard partySnapshot.exists else {
+                DispatchQueue.main.async {
+                    self.errorMessage = "Party does not exist"
+                }
+                return
+            }
+            
+            let userRef = partyRef.collection("Users").document(userID)
+            try await userRef.setData(["userID": userID, "isLeader": false])
+            
+            DispatchQueue.main.async {
+                self.errorMessage = nil
+            }
+        } catch {
+            print("Error joining party: \(error.localizedDescription)")
+            DispatchQueue.main.async {
+                self.errorMessage = "Failed to join the party: \(error.localizedDescription)"
+            }
+        }
+    }
+}
+
+
+struct JoinPartyView: View {
+    @StateObject private var viewModel = JoinPartyViewModel()
+    @State private var navigateToPartyView = false
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                // Background gradient
+                LinearGradient(gradient: Gradient(colors: [Color(red: 1.0, green: 0.9, blue: 0.8), Color(red: 1.0, green: 0.7, blue: 0.6)]), startPoint: .top, endPoint: .bottom)
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack(spacing: 30) {
+                    TextField("Enter Party Code", text: $viewModel.partyCode)
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.white))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                        .textFieldStyle(PlainTextFieldStyle())
+                        .frame(height: 50)
+                        .frame(maxWidth: 350)
+                    
+                    if let errorMessage = viewModel.errorMessage {
+                        Text(errorMessage)
+                            .foregroundColor(.red)
+                            .padding()
+                    }
+                    
+                    Button(action: {
+                        Task {
+                            await viewModel.joinParty()
+                            if viewModel.errorMessage == nil {
+                                navigateToPartyView = true
+                            }
+                        }
+                    }) {
+                        Text("Join Party")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .frame(height: 55)
+                            .frame(maxWidth: 150)
+                            .background(
+                                LinearGradient(gradient: Gradient(colors: [Color.orange, Color(red: 1.0, green: 0.5, blue: 0.0)]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                            )
+                            .cornerRadius(20)
+                            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 5, y: 5)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(Color.white, lineWidth: 2)
+                            )
+                    }
+                    
+                    
+                    
+                    NavigationLink(destination: PartyView(partyCode: viewModel.partyCode), isActive: $navigateToPartyView) {
+                        EmptyView()
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+#Preview {
+    JoinPartyView()
+}
